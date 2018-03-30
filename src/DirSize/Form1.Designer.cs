e6f41031f52ca120e490a69380ea940c712c3b3d
RichoM <richi.moran@gmail.com>
2018-03-30 20:05:03 -0300

    Added the old DirSize project.

---
+++ src/DirSize/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace DirSize
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.analyzeButton = new System.Windows.Forms.Button();
+            this.scannerWorker = new System.ComponentModel.BackgroundWorker();
+            this.entryCountLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.currentFileLabel = new System.Windows.Forms.Label();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // analyzeButton
+            // 
+            this.analyzeButton.Location = new System.Drawing.Point(13, 13);
+            this.analyzeButton.Name = "analyzeButton";
+            this.analyzeButton.Size = new System.Drawing.Size(220, 38);
+            this.analyzeButton.TabIndex = 0;
+            this.analyzeButton.Text = "Analyze folder...";
+            this.analyzeButton.UseVisualStyleBackColor = true;
+            this.analyzeButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // scannerWorker
+            // 
+            this.scannerWorker.WorkerSupportsCancellation = true;
+            this.scannerWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.scannerWorker_DoWork);
+            // 
+            // entryCountLabel
+            // 
+            this.entryCountLabel.AutoSize = true;
+            this.entryCountLabel.Location = new System.Drawing.Point(12, 65);
+            this.entryCountLabel.Name = "entryCountLabel";
+            this.entryCountLabel.Size = new System.Drawing.Size(35, 13);
+            this.entryCountLabel.TabIndex = 1;
+            this.entryCountLabel.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // currentFileLabel
+            // 
+            this.currentFileLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.currentFileLabel.AutoEllipsis = true;
+            this.currentFileLabel.Location = new System.Drawing.Point(12, 88);
+            this.currentFileLabel.Name = "currentFileLabel";
+            this.currentFileLabel.Size = new System.Drawing.Size(528, 13);
+            this.currentFileLabel.TabIndex = 2;
+            this.currentFileLabel.Text = "label1";
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(239, 13);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(220, 38);
+            this.stopButton.TabIndex = 3;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 126);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.currentFileLabel);
+            this.Controls.Add(this.entryCountLabel);
+            this.Controls.Add(this.analyzeButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button analyzeButton;
+        private System.ComponentModel.BackgroundWorker scannerWorker;
+        private System.Windows.Forms.Label entryCountLabel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label currentFileLabel;
+        private System.Windows.Forms.Button stopButton;
+    }
+}
+
---
+++ src/DirSize/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using FileSystemExtensions;
+using System.IO;
+
+namespace DirSize
+{
+
+    public partial class Form1 : Form
+    {
+
+        public class DirectoryEntry
+        {
+            public DirectoryInfo Directory { get; set; }
+            public long Length { get; set; }
+
+            public string DirectoryCSVName
+            {
+                get
+                {
+                    if (Directory.FullName.Contains(','))
+                    {
+                        return "\"" + Directory.FullName + "\"";
+                    }
+                    else
+                    {
+                        return Directory.FullName;
+                    }
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private DirectoryInfo selectedDirectory;
+        private FileInfo currentFile;
+        private long fileCount;
+        private Dictionary<string, DirectoryEntry> entries;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            selectedDirectory = null;
+            currentFile = null;
+            fileCount = 0;
+            entries = new Dictionary<string, DirectoryEntry>(StringComparer.OrdinalIgnoreCase);
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.ShowDialog();
+            if (Directory.Exists(dialog.SelectedPath))
+            {
+                selectedDirectory = new DirectoryInfo(dialog.SelectedPath);
+                scannerWorker.RunWorkerAsync();
+            }
+        }
+
+        private void scannerWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                foreach (FileInfo file in selectedDirectory.SafeEnumerateFiles("*", SearchOption.AllDirectories))
+                {
+                    fileCount++;
+                    currentFile = file;
+                    try
+                    {
+                        UpdateEntriesFor(file);
+                    }
+                    catch { /* Ignore */ }
+                    if (scannerWorker.CancellationPending) break;
+                }
+            }
+            finally
+            {
+                WriteResultsToFile();
+            }
+        }
+
+        private void UpdateEntriesFor(FileInfo file)
+        {
+            DirectoryInfo directory = file.Directory;
+            do
+            {
+                if (!entries.ContainsKey(directory.FullName))
+                {
+                    entries[directory.FullName] = new DirectoryEntry()
+                    {
+                        Directory = file.Directory,
+                        Length = 0
+                    };
+                }
+                entries[directory.FullName].Length += file.Length;
+
+                directory = directory.Parent;
+            }
+            while (directory != null && 
+                (selectedDirectory.PathEqual(directory) || 
+                selectedDirectory.Contains(directory)));
+        }
+
+        private void WriteResultsToFile()
+        {
+            string fileName;
+            int index = 0;
+            do
+            {
+                fileName = string.Format("results.{0}.{1}.csv", DateTime.Now.ToString("yyyyMMdd"), ++index);
+            }
+            while (File.Exists(fileName));
+
+            using (StreamWriter writer = new StreamWriter(fileName, false))
+            {
+                writer.WriteLine("directory,size (bytes),size (kB),size (MB), size (GB)");
+                foreach (DirectoryEntry entry in entries.Values.OrderByDescending(each => each.Length).ToArray())
+                {
+                    writer.WriteLine(string.Format("{0},{1},{2},{3},{4}",
+                        entry.DirectoryCSVName,
+                        entry.Length,
+                        entry.Length / 1024.0,
+                        entry.Length / 1024.0 / 1024.0,
+                        entry.Length / 1024.0 / 1024.0 / 1024.0));
+                }
+                writer.Flush();
+            }
+
+            MessageBox.Show(string.Format("Results written to: {0}", fileName));
+            currentFile = null;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            entryCountLabel.Text = string.Format("Files: {0}, Folders: {1}", fileCount, entries != null ? entries.Count : 0);
+            if (currentFile == null)
+            {
+                currentFileLabel.Text = "";
+            }
+            else
+            {
+                currentFile.SafeFullNameDo(name => currentFileLabel.Text = name);
+            }
+            analyzeButton.Enabled = !scannerWorker.IsBusy;
+            stopButton.Enabled = scannerWorker.IsBusy && !scannerWorker.CancellationPending;
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            scannerWorker.CancelAsync();
+        }
+    }
+}
